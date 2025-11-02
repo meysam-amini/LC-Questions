39db7efbf8c04ae68201bba9d740af366763e5e9
M.Amini <meysamamini7394@gmail.com>
2025-11-02 20:38:19 +0100

    add junit

--- src/main/java/com/meysam/lc/_1/twoSum.java
+++ src/main/java/com/meysam/lc/_1/twoSum.java
@@ -1,9 +1,40 @@
 package com.meysam.lc._1;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 class twoSum {
+
+
+    @Test
+    void example1() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, twoSum(nums, target));
+    }
+
+    @Test
+    void example2() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] expected = {1, 2};
+        assertArrayEquals(expected, twoSum(nums, target));
+    }
+
+    @Test
+    void example3() {
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, twoSum(nums, target));
+    }
+
+
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
 
--- src/main/java/com/meysam/lc/_2/CountUnguarded.java
+++ src/main/java/com/meysam/lc/_2/CountUnguarded.java
@@ -1,6 +1,35 @@
 package com.meysam.lc._2;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CountUnguarded {
 
+    @Test
+     void testExample1() {
+        int m = 4, n = 6;
+        int[][] guards = {{0, 0}, {1, 1}, {2, 3}};
+        int[][] walls = {{0, 1}, {2, 2}, {1, 4}};
+
+        int result = countUnguarded(m, n, guards, walls);
+        assertEquals(7, result);
+    }
+
+    @Test
+     void testExample2() {
+        int m = 3, n = 3;
+        int[][] guards = {{1, 1}};
+        int[][] walls = {{0, 1}, {1, 0}, {2, 1}, {1, 2}};
+
+        int result = countUnguarded(m, n, guards, walls);
+        assertEquals(4, result);
+    }
+
+
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+
+        return -1;
+    }
 
 }
